66c9c6b1907dac425b46c6a8217073148049f9c9
domrout <dom.rout@gmail.com>
2012-05-17 15:20:40 +0100

    Added support of JSON formatted files

--- src/edu/cmu/cs/lti/ark/tweetnlp/RunPOSTagger.java
+++ src/edu/cmu/cs/lti/ark/tweetnlp/RunPOSTagger.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 import edu.cmu.cs.lti.ark.ssl.pos.POSOptions;
 import edu.cmu.cs.lti.ark.ssl.pos.SemiSupervisedPOSTagger;
 import fig.basic.OptionsParser;
 import fig.basic.Option;
 
+import org.json.*;
 /**
  * Runs the POS tagger on raw tweet text -- unlike SemiSupervisedPOSTagger which does training and testing on CoNLL-formatted, pre-tokenized text.
  * 
@@ -58,11 +60,19 @@ public class RunPOSTagger {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(Opts.input), "UTF-8"));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(Opts.output), "UTF-8"));
 		String line;
-
+		StringBuilder jsonOutputText;
+		
+		JSONObject tweetJSON = null;
+		
 		int renewEvery=10000;
 		int lineNumber=0;
 		
 		while((line = reader.readLine()) != null) {
+			
+			if (Opts.input_format.equals("json")) {
+				tweetJSON = new JSONObject(line);
+				line = tweetJSON.getString("text");
+			}
 
 			// Re-read tagger every n lines to keep memory in check.
 			if (++lineNumber % renewEvery == 0) {
@@ -76,6 +86,18 @@ public class RunPOSTagger {
 					writer.write(toks.get(i) + "\t" + tags.get(i) + "\n");
 				}
 				writer.write("\n");
+				writer.flush();
+			} else if (Opts.input_format.equals("json") && Opts.format.equals("json")) {
+				jsonOutputText = new StringBuilder();
+				
+				for (int i=0; i < toks.size(); i++) {
+					jsonOutputText.append(toks.get(i) + "/" + tags.get(i) + " ");
+				}							
+				
+				tweetJSON.put("output_text", jsonOutputText);
+				
+				writer.write(tweetJSON.toString());
+				writer.write('\n');
 				writer.flush();
 			} else {
                 throw new RuntimeException("Unknown output format " + Opts.format);
