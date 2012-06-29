2db61f2bbfb8c8914c74115c6c67ac2136ec40a3
Tobi <Tobi>
2012-06-29 11:20:39 -0400

    classwrap.sh Windows-friendly
    classwrap.sh should work on Windows if you're using Cygwin, MinGW, or
    Git-Bash.  It probably won't work if you're using win-bash.

--- scripts/classwrap.sh
+++ scripts/classwrap.sh
@@ -11,9 +11,13 @@ cp=$cp:$root/bin
 cp=$cp:$root/out/production/ark-tweet-nlp
 # our build dir from "compile.sh"
 cp=$cp:$root/mybuild
-
+cp=$cp:$root/ark-tweet-nlp/target/classes
 # Jar dependencies
 cp=$cp:$(find $root/lib $root/ark-tweet-nlp/target -name '*.jar' | tr '\n' :)
-
+case $OSTYPE in
+cygwin*|msys)
+	cp=$(echo $cp | tr ':' ';')
+	;;
+esac
+set -x
 exec java -ea -cp "$cp" "$@"
-
