ea02fa69503a70686c8e40cbb913c3c59f1c1039
Brendan O'Connor <brenocon@gmail.com>
2012-09-19 03:37:55 -0400

    changes file, release tweaks

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 VERSION=0.3
-DIR=ark-tweet-nlp-$VERSION
+DIR=release/ark-tweet-nlp-$VERSION
 
 set -eux
 
-rm -rf $DIR
-mkdir $DIR
+rm -rf $DIR $DIR.zip
+mkdir -p $DIR
 
-# mvn clean
-# mvn package
+mvn clean
+mvn package
+
 cp ark-tweet-nlp/target/bin/ark-tweet-nlp-${VERSION}.jar $DIR
 
 cp -r examples $DIR
 cp -r scripts $DIR
+cp -r docs $DIR
 rm $DIR/scripts/prepare_release.sh
 rm $DIR/scripts/java.sh
+rm -f $DIR/**/.*un~
 cp *.sh $DIR
-cp *.txt $DIR
+cp {README,LICENSE}.txt $DIR
 
 # these dont work, need to fix
 rm $DIR/examples/barackobama*
+
+d=$(basename $DIR)
+(cd $(dirname $DIR) && zip -r $d.zip $d)
+
